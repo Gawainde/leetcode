e455a9b0ab158c14a9c1a6ba91b455aba6f381c0
Gawaind <Gawaind@proton.me>
2024-11-29 01:34:20 +0800

    1

---
+++ src/leetcode/AddTwoNumbers.java
@@ -0,0 +1,75 @@
+package leetcode;
+
+@SuppressWarnings("all")
+public class AddTwoNumbers {
+
+    //给你两个非空的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+    //请你将两个数相加，并以相同形式返回一个表示和的链表。
+    //你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+    // 342 + 465
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l11 = new ListNode(4);
+        l1.next = l11;
+        l11.next = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        ListNode l22 = new ListNode(6);
+        l2.next = l22;
+        l22.next = new ListNode(4);
+        System.out.println(addTwoNumbers(l1, l2));
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //创建一个结果节点temp和临时节点tail
+        ListNode temp = null, tail = null;
+        int flag = 0;
+        // flag != 0 判断最高位是否需要进位 如109 + 209 当走到第三位时l1 l2的next节点均为null 但9 + 9需要进位
+        while (l1 != null || l2 != null || flag != 0) {
+            int flag1 = l1 != null ? l1.val : 0;
+            int flag2 = l2 != null ? l2.val : 0;
+            int sum = flag1 + flag2 + flag;
+            //判断进位 flag > 0则需要进一位
+            flag = sum / 10;
+            if (temp == null){
+                //结果节点temp和临时节点tail二者指向对象相同  注意：temp始终指向链表的头结点  tail后续会指向temp的下一个节点
+                temp = new ListNode(sum % 10);
+                tail = temp;
+            } else {
+                //tail.next的val为sum对10取余 （第一次循环）即temp.next的val也是该值（因为tail与temp指向的是同一个对象）
+                //（第二次循环）此时tail指向的是temp.next对象 即tail.next = temp.next.next 如此循环
+                tail.next = new ListNode(sum % 10);
+                //将tail指向temp.next对象
+                tail = tail.next;
+            }
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return temp;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
---
+++ src/leetcode/TwoSum.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 10, 7, 11, 15};
+        int[] ints = twoSum(nums, 25);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    //给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            //如果存在 则代表map中存在与当前nums[i]相加为target的key 输出二者的下标即可
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            //如果不存在 记录key为当前nums[i]  value为下标
+            map.put(nums[i], i);
+        }
+        //不要忘记如果没有匹配的返回一个空数组
+        return new int[0];
+    }
+}
